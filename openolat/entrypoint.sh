bb79c706918537066279ba6f99e0cf6bd86c09bf
Oliver van Porten <oliver@van-porten.de>
2023-05-30 22:04:44 +0200

    fix olat properties

--- openolat/entrypoint.sh
+++ openolat/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Read in the file of environment settings
 . $HOME/.bashrc
+
+envsubst < /home/openolat/lib/olat.local.properties.template > /home/openolat/lib/olat.local.properties
+
 # Then run the CMD
 exec "$@"
