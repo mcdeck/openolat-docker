cbdf59406702ddf350bc55bc3326812a99f05205
Oliver van Porten <oliver@van-porten.de>
2023-05-30 22:34:30 +0200

    fix init ssl script

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,10 +8,9 @@ email="oliver@van-porten.de" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 echo "### Fetch nginx config ..."
-docker compose run --rm --entrypoint "\
-  mkdir -p '/etc/letsencrypt' && \
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > /etc/letsencrypt/options-ssl-nginx.conf && \
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > /etc/letsencrypt/ssl-dhparams.pem" nginx
+docker compose run --rm --entrypoint "/bin/sh -c '\
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf -o /etc/letsencrypt/options-ssl-nginx.conf; \
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem -o /etc/letsencrypt/ssl-dhparams.pem'" nginx
 echo
 
 echo "### Creating dummy certificate for $domains ..."
